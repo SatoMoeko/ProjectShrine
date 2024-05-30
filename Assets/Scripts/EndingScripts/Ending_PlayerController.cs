98cfb47f2225993b9ff8f7bd9c1a16fc6ba3fe9c
memyou <mem2490oyou3@gmail.com>
2024-05-30 13:31:47 +0900

    player関連のスクリプト作成

--- Assets/Scripts/EndingScripts/Ending_PlayerController.cs
+++ Assets/Scripts/EndingScripts/Ending_PlayerController.cs
@@ -3,38 +3,69 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
+
+//endingのテスト用
+//途中
 public class Ending_PlayerController : MonoBehaviour
 {
-    //endingのテスト用
-    //途中
+    //オブジェクト
+    public Transform orientation;
 
-    public float moveSpeed = 3;
+    //コンポーネント
     Rigidbody rb;
 
-    float keyHorizontal;
-    float keyVertical;
+    //移動関係
+    Vector3 velocity; //移動速度
+    Vector3 moveInput; //移動の入力値
+    Vector3 moveDirection; //実際の移動距離
+    Quaternion lookNow; //見ている方向
+    public float walkSpeed = 3f; //歩行速度
 
-    Vector3 moveDirection;
+
+    //判定関係
+    bool isGrounded; //接地しているか
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        //回転を制限
+        // rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
         rb.freezeRotation = true;
     }
 
-
     void Update()
     {
+    }
 
+
+    void FixedUpdate()
+    {
+        MovePlayer();
     }
 
     void KeyChecked()
     {
-        keyHorizontal = Input.GetAxisRaw("Horizontal");
-        keyVertical = Input.GetAxisRaw("Vertical");
+
     }
 
     void MovePlayer()
     {
+        //入力値
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //向いている方向
+        moveDirection = orientation.forward * moveInput.z + orientation.right * moveInput.x;
+
+        //移動速度の計算
+        var clampedInput = Vector3.ClampMagnitude(moveDirection, 1f);
+        velocity = clampedInput * walkSpeed;
+
+        //移動速度の制限
+        velocity -= rb.velocity;
+        velocity = new Vector3(Mathf.Clamp(velocity.x, -walkSpeed, walkSpeed), 0f, Mathf.Clamp(velocity.z, -walkSpeed, walkSpeed));
+
+        //移動処理
+        rb.AddForce(rb.mass * velocity / Time.fixedDeltaTime, ForceMode.Force);
     }
+
 }
