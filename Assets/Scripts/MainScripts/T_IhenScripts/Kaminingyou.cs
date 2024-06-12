9420a8f3d4b50341abde0eca9081faa8438d481d
memyou <mem2490oyou3@gmail.com>
2024-06-12 20:00:47 +0900

    ステージ作成中

---
+++ Assets/Scripts/MainScripts/T_IhenScripts/Kaminingyou.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/*
+プレイヤーに処理を追加しないで実装できるようにする
+プレイヤーがキャスト内に侵入したら、UIではがす指示表示する
+はがされたらオブジェクト破棄してはがした数カウント
+*/
+
+public class Kaminingyou : MonoBehaviour, IInteractable
+{
+    // public TextMeshProUGUI peelOffText;
+
+    //キャスト用情報
+    public float radius; //キャスト半径
+    public LayerMask targetLayer; //キャストが取得するレイヤー：player
+
+    public Collider[] result = new Collider[1]; //取得するコライダー,プレイヤーのみなので１でOK
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        int l = Physics.OverlapSphereNonAlloc(transform.position, radius, result, targetLayer);
+        switch (l)
+        {
+            //プレイヤーが範囲内に入っている時:はがせる
+            case 1:
+                // Debug.Log("player接近中");
+                Interact();
+                break;
+            //プレイヤーが範囲外にいるとき:はがせない
+            default:
+                // Debug.Log("player離脱中");
+                // peelOffText.enabled = false;
+                break;
+        }
+
+    }
+
+    //円の可視化
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public void Interact()
+    {
+        //インタラクト時の処理、はがされたらオブジェクト破棄
+        // peelOffText.enabled = true;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("はがした");
+            KaminingyouDirector.count--;
+            Destroy(gameObject);
+        }
+
+    }
+}
